d096328376ca3ff2cf431e278331d0c198825da8
Test User <test@example.com>
2025-08-11 23:20:39 -0300

    feat(S3/DynamoDb/ECR/AppRunner) - @GustavoCarvalho25 - Adicionando conexão e integrando execução com AppRunner

---
+++ formacao-aws-2-01-pos-rds/JobManager.API/Models/JobDbModel.cs
@@ -0,0 +1,37 @@
+using Amazon.DynamoDBv2.DataModel;
+using JobManager.API.Entities;
+
+namespace JobManager.API.Models;
+
+[DynamoDBTable("Jobs")]
+public class JobDbModel
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal MinSalary { get; set; }
+    public decimal MaxSalary { get; set; }
+    public string Company { get; set; }
+    public List<JobApplicationDbModel> Applications { get; set; }
+
+    public static JobDbModel FromEntity(Job job)
+        => new JobDbModel
+        {
+            Id = Guid.NewGuid().ToString(),
+            Title = job.Title,
+            Description = job.Description,
+            MinSalary = job.MinSalary,
+            MaxSalary = job.MaxSalary,
+            Company = job.Company,
+            Applications = []
+        };
+}
+
+
+public class JobApplicationDbModel
+{
+    public string Id { get; set; }
+    public string CandidateName { get; set; }
+    public string CandidateEmail { get; set; }
+    public string? CVUrl { get; set; }
+}
--- formacao-aws-2-01-pos-rds/JobManager.API/Program.cs
+++ formacao-aws-2-01-pos-rds/JobManager.API/Program.cs
@@ -1,10 +1,13 @@
 using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Extensions.NETCore.Setup;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using JobManager.API.Entities;
+using JobManager.API.Models;
 using JobManager.API.Persistence;
 using JobManager.API.Workers;
 using Microsoft.AspNetCore.Mvc;
@@ -40,7 +43,11 @@ internal class Program
         builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(connectionString));
 
         builder.Services.AddHostedService<JobApplicationNotificationWorker>();
-
+        
+        var dynamoDbClient = new AmazonDynamoDBClient(RegionEndpoint.SAEast1);
+        
+        builder.Services.AddSingleton<IAmazonDynamoDB>(dynamoDbClient);
+        
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
@@ -184,6 +191,142 @@ internal class Program
             
             return Results.File(response.ResponseStream, response.Headers.ContentType, response.Headers.ContentDisposition);
         });
+        
+        
+        
+        //---------------------------------------------------------------- [ API - V2 ] ----------------------------------------------------------------
+        
+        app.MapPost("/api/v2/jobs", async (Job job) =>
+        {
+            var db = new DynamoDBContext(dynamoDbClient);
+
+            var model = JobDbModel.FromEntity(job);
+            
+            await db.SaveAsync(model);
+
+            return Results.Created($"/api/jobs/{job.Id}", job);
+        });
+        
+        app.MapGet("/api/v2/jobs", async () =>
+        {
+            var db = new DynamoDBContext(dynamoDbClient);
+            
+            var jobs = await db.ScanAsync<JobDbModel>([]).GetRemainingAsync();
+
+            return Results.Ok(jobs);
+        });
+        
+        app.MapGet("/api/v2/jobs/{id}", async (string id) =>
+        {
+            var db = new DynamoDBContext(dynamoDbClient);
+            
+            var job = await db.LoadAsync<JobDbModel>(id);
+
+            if (job is null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(job);
+        });
+        
+        app.MapPost("/api/v2/jobs/{id}/job-applications", async (int id, JobApplication application, [FromServices] IConfiguration configuration) =>
+        {
+            var db = new DynamoDBContext(dynamoDbClient);
+            
+            var job = await db.LoadAsync<JobDbModel>(id.ToString());
+
+            if (job is null)
+            {
+                return Results.NotFound();
+            }
+
+            application.JobId = id;
+
+            var jobApplicationModel = new JobApplicationDbModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                CandidateName = application.CandidateName,
+                CandidateEmail = application.CandidateEmail,
+                CVUrl = application.CVUrl,
+            };
+            
+            
+            job.Applications.Add(jobApplicationModel);
+
+            await db.SaveAsync(job);
+            
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            
+            var queueUrl = configuration.GetValue<string>("AWS:SQSQueueUrl") ?? string.Empty;
+            
+            var queueResponse = await client.GetQueueUrlAsync("FormacaoAwsGustavoC");
+            
+            var queue = queueResponse.QueueUrl;
+            
+            var request = new SendMessageRequest
+            {
+                QueueUrl = queue,
+                MessageBody = $"New application for job {id} by {application.CandidateName} ({application.CandidateEmail})"
+            };
+            
+            var result = await client.SendMessageAsync(request);
+            
+            return Results.NoContent();
+        });
+        
+        app.MapPut("/api/v2/job/{id}/job-applications/{applicationId}/upload-cv", async (string id, string applicationId, IFormFile file, [FromServices] IConfiguration configuration) =>
+        {
+            if (file == null || file.Length == 0)
+            {
+                return Results.BadRequest();
+            }
+
+            var extension = Path.GetExtension(file.FileName);
+
+            var validExtensions = new List<string> { ".pdf", ".docx" };
+
+            if (!validExtensions.Contains(extension))
+            {
+                return Results.BadRequest();
+            }
+            
+            var client = new AmazonS3Client(RegionEndpoint.SAEast1);
+
+            var bucketName = configuration.GetValue<string>("AWS:S3BucketName");
+            
+            var key = $"job-applications/{applicationId}-{file.FileName}";
+
+            using var stream = file.OpenReadStream();
+
+            var putObject = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = key,
+                InputStream = stream
+            };
+            
+            var db = new DynamoDBContext(dynamoDbClient);
+
+            
+            var response = await client.PutObjectAsync(putObject);
+
+            var job = await db.LoadAsync<JobDbModel>(id);
+            
+            var application = job.Applications.SingleOrDefault(a => a.Id == applicationId);
+
+            if (application is null)
+            {
+                return Results.NotFound();
+            }
+
+            application.CVUrl = key;
+
+            await db.SaveAsync(job);
+
+            return Results.NoContent();
+        }).DisableAntiforgery();
+        
         app.Run();
     }
 }
